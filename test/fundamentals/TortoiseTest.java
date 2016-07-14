99c020183ad96eff3ad348eb5b342dd73bc37df7
DGovorov <heavenbox@mail.ru>
2016-07-14 22:08:10 +0300

    Tortoise racing test

---
+++ test/fundamentals/TortoiseTest.java
@@ -0,0 +1,20 @@
+package fundamentals;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by Dim on 14.07.2016.
+ */
+public class TortoiseTest {
+
+    @Test
+    public void test1() {
+        System.out.println("Basic Tests");
+        assertArrayEquals(new int[]{0, 32, 18}, Tortoise.race(720, 850, 70));
+        assertArrayEquals(new int[]{3, 21, 49}, Tortoise.race(80, 91, 37));
+        assertArrayEquals(new int[]{2, 0, 0}, Tortoise.race(80, 100, 40));
+    }
+}
